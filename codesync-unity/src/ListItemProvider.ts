9d793297b4919133a1011d83313c5d6ad3994db3
Mayank Madan <madanmayank10@gmail.com>
2024-08-20 19:34:19 -0300

    Added ListItemProvider

---
+++ codesync-unity/src/ListItemProvider.ts
@@ -0,0 +1,25 @@
+import * as vscode from 'vscode';
+
+export class ListItemProvider implements vscode.TreeDataProvider<MyTreeItem> {
+  private _onDidChangeTreeData: vscode.EventEmitter<MyTreeItem | undefined> = new vscode.EventEmitter<MyTreeItem | undefined>();
+  readonly onDidChangeTreeData: vscode.Event<MyTreeItem | undefined> = this._onDidChangeTreeData.event;
+
+  getTreeItem(element: MyTreeItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: MyTreeItem): Thenable<MyTreeItem[]> {
+    // Return the children of the current node (or root nodes if `element` is undefined)
+    return Promise.resolve([
+      new MyTreeItem('Item 1'),
+      new MyTreeItem('Item 2'),
+      new MyTreeItem('Item 3')
+    ]);
+  }
+}
+
+export class MyTreeItem extends vscode.TreeItem {
+  constructor(label: string) {
+    super(label, vscode.TreeItemCollapsibleState.None);
+  }
+}
--- codesync-unity/src/extension.ts
+++ codesync-unity/src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { ListItemProvider } from './ListItemProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -19,8 +20,10 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInformationMessage('Hello World from CodeSync Unity!');
 	});
 
+	vscode.window.registerTreeDataProvider('codesync-list', new ListItemProvider());
+
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
